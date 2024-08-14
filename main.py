d5543bdcad83a54f1477bae9f831ad1693ad9f9d
kikuuchi <kikuuchi@itcowork.co.jp>
2024-08-14 21:24:40 +0900

    feat: import 処理の実装

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ logging.basicConfig(
     filename="app.log", level=logging.DEBUG, format="%(asctime)s %(message)s"
 )
 
+from routers import Area, Attendance, Auth, User, UserLocation, UserStatus
+
 app = FastAPI()
 @app.get("/")
 def read_root():
