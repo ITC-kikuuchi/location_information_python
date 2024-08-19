d36cae920a950e4f473a978362bbeefaba08592e
kikuuchi <kikuuchi@itcowork.co.jp>
2024-08-19 15:01:01 +0900

    feat: ユーザ登録処理の実装

--- cruds/User.py
+++ cruds/User.py
@@ -6,3 +6,14 @@ import models.m_user as UserModel
 # ユーザ一覧取得
 def getUsers(db: Session):
     return db.query(UserModel.User).all()
+
+# ユーザ登録
+def createUser(db: Session, user, loginUser):
+    createUser = UserModel.User(
+        **user,
+        created_id=loginUser.id,
+        updated_id=loginUser.id
+    )
+    db.add(createUser)
+    db.commit()
+    db.refresh(createUser)
