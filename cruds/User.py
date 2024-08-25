a867e612412fe1528232368e7ad6b752eb5e8876
kikuuchi <kikuuchi@itcowork.co.jp>
2024-08-25 23:09:29 +0900

    feat: ユーザ更新処理の実装

--- cruds/User.py
+++ cruds/User.py
@@ -21,3 +21,12 @@ def createUser(db: Session, user, loginUser):
 # ユーザ詳細取得
 def getUserDetail(db: Session, userId: int):
     return db.query(UserModel.User).filter(UserModel.User.id == userId).first()
+
+
+# ユーザ更新
+def updateUser(db: Session, user, loginUser, original: UserModel.User):
+    for field, value in user.items():
+        setattr(original, field, value)
+
+    original.update_id = loginUser.id
+    db.commit()
