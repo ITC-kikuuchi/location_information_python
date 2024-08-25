995ddeda6cef61f9da8fbc09c22f34685000fe38
kikuuchi <kikuuchi@itcowork.co.jp>
2024-08-25 23:09:42 +0900

    feat: ユーザ更新API の実装

--- routers/User.py
+++ routers/User.py
@@ -87,9 +87,39 @@ def getUserDetail(
 
 
 # ユーザ更新API
-@router.put("/users/{users_id}")
-def updateUser():
-    pass
+@router.put("/users/{user_id}")
+def updateUser(
+    user_id: int,
+    item: UserSchema.updateUser,
+    loginUser: dict = Depends(getCurrentUser),
+    db: Session = Depends(get_db),
+):
+    try:
+        # 権限チェック
+        CheckExecutionAuthority(loginUser, user_id)
+        # ユーザID に紐づくデータの取得
+        user = UserCrud.getUserDetail(db, user_id)
+        if not user:
+            # id に紐づくデータが存在しなかった場合
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="NotFound"
+            )
+        # 更新データの作成
+        updateUser = {
+            "user_name": item.user_name,
+            "user_name_kana": item.user_name_kana,
+            "mail_address": item.mail_address,
+            "is_admin": item.is_admin,
+            "default_area_id": item.default_area_id,
+            "password": item.password if item.password else user.password
+        }
+        # ユーザデータ更新処理
+        UserCrud.updateUser(db, updateUser, loginUser, user)
+        return {"message": "Operation completed successfully"}
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 # ユーザ削除API
