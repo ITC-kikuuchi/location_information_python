d7e29927337244728f1dcf8c12079ddd2c58518e
kikuuchi <kikuuchi@itcowork.co.jp>
2024-09-01 22:39:56 +0900

    feat: 管理者権限チェック処理の実装

--- routers/Area.py
+++ routers/Area.py
@@ -10,6 +10,18 @@ import schemas.Area as AreaSchema
 
 router = APIRouter()
 
+# 管理者権限及びIDチェック
+def CheckExecutionAuthority(loginUser, user_id = None):
+    # 権限チェック
+    if not loginUser.is_admin:
+        # 管理者権限が存在しない場合
+        if not user_id or loginUser.id != user_id:
+            # ユーザID が存在しない、またはログインユーザのID と一致しない場合
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden"
+            )
+
+
 # エリア一覧取得API
 @router.get("/areas", response_model=Dict[str, list[AreaSchema.getAreas]])
 def getAreas(loginUser: dict = Depends(getCurrentUser), db: Session = Depends(get_db)):
