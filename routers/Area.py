8095781cc0f38d10cdb27c6f45a7b68cf5b44c00
kikuuchi <kikuuchi@itcowork.co.jp>
2024-09-01 22:40:20 +0900

    feat: エリア登録API の実装

--- routers/Area.py
+++ routers/Area.py
@@ -37,8 +37,27 @@ def getAreas(loginUser: dict = Depends(getCurrentUser), db: Session = Depends(ge
 
 # エリア登録API
 @router.post("/areas")
-def createArea():
-    pass
+def createArea(
+    item: AreaSchema.createArea,
+    loginUser: dict = Depends(getCurrentUser),
+    db: Session = Depends(get_db),
+):
+    try:
+        # 権限チェック
+        CheckExecutionAuthority(loginUser)
+        # 登録データの作成
+        area = {
+            "area_name": item.area_name,
+            "is_default_area": item.is_default_area,
+        }
+        # ユーザデータ登録処理
+        AreaCrud.createArea(db, area, loginUser)
+        # OK レスポンス
+        return {"message": "Operation completed successfully"}
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 # エリア詳細取得API
 @router.get("/areas/{area_id}")
