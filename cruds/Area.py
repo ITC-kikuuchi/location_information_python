d5d6ebb636848ad2c7ab5a0631534e76300bb847
kikuuchi <kikuuchi@itcowork.co.jp>
2024-09-05 14:25:02 +0900

    feat: エリア更新処理の実装

--- cruds/Area.py
+++ cruds/Area.py
@@ -21,3 +21,12 @@ def createArea(db: Session, area, loginUser):
 # エリア詳細取得
 def getAreaDetail(db: Session, areaId: int):
     return db.query(AreaModel.Area).filter(AreaModel.Area.id == areaId).first()
+
+
+# エリア更新
+def updateArea(db: Session, area, loginUser, original: AreaModel.Area):
+    for field, value in area.items():
+        setattr(original, field, value)
+
+    original.update_id = loginUser.id
+    db.commit()
