f793acc8c8d108ef2d4ee7e4201c128adf521781
kikuuchi <kikuuchi@itcowork.co.jp>
2024-09-08 21:12:16 +0900

    feat: 勤怠状況一覧取得API のレスポンススキーマの実装

--- schemas/Attendance.py
+++ schemas/Attendance.py
@@ -2,3 +2,10 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+
+class getAttendances(BaseModel):
+    id: int
+    attendance_status: Optional[str]
+
+    class Config:
+        orm_mode = True
