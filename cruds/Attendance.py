9b117e2105e07be96444c04fcf5978e728b1925c
kikuuchi <kikuuchi@itcowork.co.jp>
2024-09-08 21:12:56 +0900

    feat: 勤怠状況一覧取得処理の実装

--- cruds/Attendance.py
+++ cruds/Attendance.py
@@ -2,3 +2,7 @@ from sqlalchemy.orm import Session
 
 import models.m_attendance as AttendanceModel
 
+
+# 勤怠状況一覧取得
+def getAttendances(db: Session):
+    return db.query(AttendanceModel.Attendance).all()
