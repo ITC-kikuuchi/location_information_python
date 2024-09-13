9c9b07c7c7e77a7fe2326ea8505b33e6ada57f57
kikuuchi <kikuuchi@itcowork.co.jp>
2024-09-13 19:39:28 +0900

    feat: ユーザーステータス一覧取得API の実装

--- routers/UserStatus.py
+++ routers/UserStatus.py
@@ -11,6 +11,18 @@ import schemas.UserStatus as UserStatusSchema
 router = APIRouter()
 
 # ユーザステータス一覧取得API
-@router.get("/user-status")
-def getUserStatus():
-    pass
+@router.get(
+    "/user-status", response_model=Dict[str, list[UserStatusSchema.getUserStatus]]
+)
+def getUserStatus(
+    loginUser: dict = Depends(getCurrentUser), db: Session = Depends(get_db)
+):
+    try:
+        # ユーザステータス一覧取得
+        user_status = {"user_status_list": UserStatusCrud.getUserStatus(db)}
+        # OK レスポンス
+        return user_status
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
