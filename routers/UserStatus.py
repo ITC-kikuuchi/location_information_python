02c7aaaaa16b84f3e36ec3caa5434e1ba42a0e89
kikuuchi <kikuuchi@itcowork.co.jp>
2024-09-13 19:39:09 +0900

    feat: import 処理の実装

--- routers/UserStatus.py
+++ routers/UserStatus.py
@@ -1,4 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import Dict
+from database import get_db
+from routers.Auth import getCurrentUser
+
+
+import cruds.UserStatus as UserStatusCrud
+import schemas.UserStatus as UserStatusSchema
 
 router = APIRouter()
 
